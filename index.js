caa3fb62a71748bae6efeaaf554ef5e4be3fd94a
Yevgeniy <yevgeniyerot@gmail.com>
2019-05-02 10:31:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+// let express = require('express');
+
+class Cat {
+
+	constructor(name, sex) {
+		this.name = name;
+		this.sex = sex;
+	}
+
+	speak() {
+		return this.name + ' says meow!';
+	}
+}
+
+class Dog {
+
+	constructor(name, sex) {
+		this.name = name;
+		this.sex = sex;
+	}
+
+	speak() {
+		return this.name + ' says woof!';
+	}
+}
+
+class Bird {
+
+	constructor(name, sex) {
+		this.name = name;
+		this.sex = sex;
+	}
+
+	speak() {
+		if (this.sex === 'male') {
+			return "It's me! " + this.name + ", the parrot!"
+		} else {
+			return 'Mable says squawk!';
+		}
+	}
+}
